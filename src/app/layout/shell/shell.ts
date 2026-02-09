5380af2e1b01f79daf7f4a706cb19da0a81bb471
Timothy Brooke <brooketimothy494@gmail.com>
2026-02-09 11:55:51 +0100

    Updated Navbar BUG 3

--- src/app/layout/shell/shell.ts
+++ src/app/layout/shell/shell.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
-import {NavbarComponent } from '../navbar/navbar';
-import { Footer } from '../footer/footer';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-shell',
-  imports: [NavbarComponent, Footer, RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './shell.html',
-  styleUrl: './shell.css',
+  styleUrls: ['./shell.css']
 })
 export class Shell {
-
+  constructor(private router: Router, private viewport: ViewportScroller) {
+    this.router.events
+      .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
+      .subscribe(() => {
+        setTimeout(() => this.viewport.scrollToPosition([0, 0]));
+      });
+  }
 }

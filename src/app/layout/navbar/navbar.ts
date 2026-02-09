0b6a3f67819d7f9ad3030c00f3d4cb508fc7e1dc
Timothy Brooke <brooketimothy494@gmail.com>
2026-02-09 11:34:19 +0100

    Updated Navbar BUG 3

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,31 +1,12 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Router, NavigationEnd, RouterModule } from '@angular/router';
-import * as bootstrap from 'bootstrap';
-import { filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
-  imports: [RouterModule]
+  styleUrl: './navbar.css'
 })
 export class NavbarComponent {
-  @ViewChild('mainNavCollapse', { static: false }) mainNavCollapse?: ElementRef<HTMLElement>;
-
-  constructor(private router: Router) {
-    // Close menu on route change as well (covers back/forward, programmatic nav, etc.)
-    this.router.events.pipe(filter(e => e instanceof NavigationEnd)).subscribe(() => {
-      this.closeMobileMenu();
-    });
-  }
-
-  closeMobileMenu() {
-    const el = this.mainNavCollapse?.nativeElement;
-    if (!el) return;
-
-    // Only hide if currently shown
-    if (el.classList.contains('show')) {
-      const collapse = bootstrap.Collapse.getOrCreateInstance(el, { toggle: false });
-      collapse.hide();
-    }
-  }
 }

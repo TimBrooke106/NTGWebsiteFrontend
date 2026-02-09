2b43b9ba7bc9286b93bd340b2a0b6acda9460119
Timothy Brooke <brooketimothy494@gmail.com>
2026-02-09 11:17:22 +0100

    Updated Navbar BUG

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -7,7 +7,7 @@ import { filter } from 'rxjs/operators';
   selector: 'app-navbar',
   templateUrl: './navbar.html'
 })
-export class navbar {
+export class NavbarComponent {
   @ViewChild('mainNavCollapse', { static: false }) mainNavCollapse?: ElementRef<HTMLElement>;
 
   constructor(private router: Router) {

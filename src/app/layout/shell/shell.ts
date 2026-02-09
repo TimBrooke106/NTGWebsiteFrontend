7f05f0a28786e17637373ddb60ae5da76bc8a377
Timothy Brooke <brooketimothy494@gmail.com>
2026-02-09 12:02:08 +0100

    Updated Navbar BUG 3

--- src/app/layout/shell/shell.ts
+++ src/app/layout/shell/shell.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
-import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
-import { ViewportScroller } from '@angular/common';
-import { filter } from 'rxjs/operators';
-import { NavbarComponent } from "../navbar/navbar";
-import { Footer } from "../footer/footer";
-
+import { Component, NgZone } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { filter, take } from 'rxjs/operators';
+import { NavbarComponent } from '../navbar/navbar';
+import { Footer } from '../footer/footer';
 @Component({
   selector: 'app-shell',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, Footer],
+  imports: [NavbarComponent, RouterOutlet, Footer],
   templateUrl: './shell.html',
-  styleUrls: ['./shell.css']
+  styleUrls: ['./shell.css'],
 })
 export class Shell {
-  constructor(private router: Router, private viewport: ViewportScroller) {
+  constructor(private router: Router, private zone: NgZone) {
     this.router.events
       .pipe(filter((e): e is NavigationEnd => e instanceof NavigationEnd))
       .subscribe(() => {
-        setTimeout(() => this.viewport.scrollToPosition([0, 0]));
+        // wait until Angular finishes rendering the new route
+        this.zone.onStable.pipe(take(1)).subscribe(() => {
+          // extra-safe: run after the browser paints
+          requestAnimationFrame(() => {
+            requestAnimationFrame(() => {
+              window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+            });
+          });
+        });
       });
   }
 }

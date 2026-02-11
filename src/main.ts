1ecab303d2411db543631da65149f086f942bb9c
Timothy Brooke <brooketimothy494@gmail.com>
2026-02-11 15:14:57 +0100

    Updated Navbar

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,27 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AdminTokenInterceptor } from './app/core/interceptors/admin-token.interceptor';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-
 bootstrapApplication(App, {
   providers: [
-      provideHttpClient(withInterceptorsFromDi()),
-  { provide: HTTP_INTERCEPTORS, useClass: AdminTokenInterceptor, multi: true },
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
+
     provideAnimationsAsync(),
-    provideHttpClient()
-    
-  ]
+
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: AdminTokenInterceptor, multi: true },
+  ],
 });
